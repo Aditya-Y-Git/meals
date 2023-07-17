02b10aef0af605fe591056eac69b70ea88a560b2
Aditya <yendralwaraditya@gmail.com>
2023-07-17 11:23:06 +0530

    created basic layout for categories screen

---
+++ lib/categories.dart
@@ -0,0 +1,34 @@
+// to display the categories of meals available
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: const [
+          Card(
+            child: Text('Card 1'),
+          ),
+          Card(
+            child: Text('Card 2'),
+          ),
+          Card(
+            child: Text('Card 3'),
+          ),
+          Card(
+            child: Text('Card 4'),
+          ),
+        ],
+      ),
+    );
+  }
+}
